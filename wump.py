764e034de13967d7eb0554e53fb0ed3cb4cd2ff7
Ryan Michalec <rmichalec14@gmail.com>
2018-07-04 01:22:15 -0400

    still sometimes infinite looping

--- wump.py
+++ wump.py
@@ -17,14 +17,21 @@ class WumpController:
 		tempList = list(range(num_rooms))
 		random.shuffle(tempList)
 
+		# list of unfull rooms 
+		unfulList = list(range(num_rooms))
+
 		# for each room, get it a valid neighbor if it has < 3 neighbors
 		for room in tempList:
 			while len(self.graph[room]) < 3: 
 				# choose another room at random; skip if self or already has three neighbors
-				test = random.choice(range(num_rooms))
+				test = random.choice(unfulList)
 				if(test != room and len(self.graph[test]) != 3):
 					self.graph[room].append(test)
 					self.graph[test].append(room)
+					if(len(self.graph[test]) == 3):
+						unfulList.remove(test)
+			if(room in unfulList):
+				unfulList.remove(room)
 
 		# choose unique starting locations for the fixed hazards 
 		random.shuffle(tempList)
@@ -41,14 +48,29 @@ class WumpController:
 			self.playerLocation = random.choice(range(num_rooms))
 
 		# debug code, remove
+		for x in self.graph:
+			print(x, ": \n", *self.graph[x])
 		for x in self.contains:
-			print(x, ": ", self.contains[x])
+			print(x, ": \n", self.contains[x])
 		print(self.playerLocation)
 		print(self.wumpLocation)
 	
+	def ReadAdjacentRooms(self, currentRoomNum):
+		output = ""
+		for adj in random.sample(self.graph[currentRoomNum], 3):
+			if(adj == self.wumpLocation):
+				output += "I smell a wumpus\n"
+			if(self.contains[adj] == "B"):
+				output += "Bats nearby\n"
+			if(self.contains[adj] == "H"):
+				output += "I feel a draft\n"
+		if(output != ""):
+			output = output[:-1]
+		return output
+
 	def Reset(self):
 		self.__init__()
-		pass
 
 if __name__ == "__main__":
 	WumpController2 = WumpController()
+	print(WumpController2.ReadAdjacentRooms(WumpController2.playerLocation))
